a4c4c662321d351e070c333fc6f2a1308e350810
JupiterPi <hello@jupiterpi.de>
2025-07-23 22:31:41 +0200

    make bun build --compile work

--- ffi/ffi.ts
+++ ffi/ffi.ts
@@ -1,6 +1,10 @@
-import { dlopen, FFIType, suffix } from "bun:ffi"
+import { dlopen, FFIType } from "bun:ffi"
 
-const lib = dlopen(`ffi/target/release/libffi.${suffix}`, {
+// @ts-ignore next-line will be replaced by Bun with path valid after bundling
+import binaryPath from "./target/ffi-binary" with { type: "file" }
+// note: build process needs to be run via `bun run build-ffi` to ensure the binary is renamed correctly
+
+const lib = dlopen(binaryPath, {
   _get_auth_crypto_key: {
     args: [FFIType.bool],
     returns: FFIType.cstring,
---
+++ ffi/rename-ffi-binary.js
@@ -0,0 +1,20 @@
+// This script renames the compiled FFI binary to a consistent name
+// It should be run after building the FFI library with `cargo build --release`
+// This is necessary to ensure that the path to the binary is consistent across different platforms
+// and can be imported with at static path from ffi.ts
+
+// rename ffi.dll
+try {
+    require("fs").renameSync("./target/release/ffi.dll", "./target/ffi-binary");
+    console.log("Renamed target/release/ffi.dll to target/ffi-binary");
+    process.exit(0);
+} catch {}
+
+// rename libffi.so
+try {
+    require("fs").renameSync("./target/release/libffi.so", "./target/libffi-binary");
+    console.log("Renamed target/release/libffi.so to target/libffi-binary");
+    process.exit(0);
+} catch {}
+
+console.error("Failed to rename ffi binary.");
